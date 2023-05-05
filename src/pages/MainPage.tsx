660a4529a8650b9423e8bff25290de334406fd40
Leonard Velásquez <leonard.avp@gmail.com>
2023-05-05 17:14:17 -0400

    Pantalla principal

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,7 +3,11 @@ import CenterBox from "../components/CenterBox"
 const MainPage = () => {
   return (
     <CenterBox>
-      <div>Hi! from center box</div>
+      <div className="container d-flex flex-column space-y-4">
+        <img width="250rem" height="250rem" className="img-thumbnail rounded" src="/logo.jpg" alt="Venemexi Fashion Logo" />
+        <button type="button" className="btn btn-primary">Cargar pedido</button>
+        <button type="button" className="btn btn-primary">Ver pedidos</button>
+      </div>
     </CenterBox>
   )
 }
